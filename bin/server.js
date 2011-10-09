d920ad5489f4b483e3d28cae2e2fd50191753544
Joshua Holbrook <josh.holbrook@gmail.com>
2011-10-08 21:31:55 -0700

    Added couch cacheing!

--- bin/server.js
+++ bin/server.js
@@ -1,20 +1,28 @@
+require("colors");
+
 var http = require("http");
-static = require('node-static');
+static = require("node-static");
 var service = require(__dirname + "/../lib/browserify-service");
+var winston = require("winston");
 
 //Static fileserver
 http.createServer(function (req, res) {
     var query;
 
     if (query = service.getQuery(req.url)) {
-        try {
-            //TODO: async! This is gonna lock up ALL the requests.
-            var js = service.getBundle(query.require);
+
+        service.getBundle(query.require, function (err, js) {
+            if (err) {
+                winston.info("Cache ".yellow + "error".red + ": "+( (err && err.message) || "status code "+resp.statusCode ).cyan);
+            }
             res.writeHead(200, {'Content-Type': 'text/javascript'});
             res.end(js);
-        } catch (e) {
+        });
+
+        function onError(err) {
             res.writeHead(500, {'Content-Type': 'text/plain' });
-            res.end(e.message);
+            winston.error("In getting bundle: ".red + e.message);
+            res.end("FLAGRANT ERROR");
         }
     } else {
         var server = new static.Server(__dirname+"/../static/");
