c631d2436e48cd4dccdb964543a422b966574436
Joshua Holbrook <josh.holbrook@gmail.com>
2013-11-24 13:24:47 -0700

    [refactor] Use custom expiry-model for inheritance (breaks tests)

--- bundler/cache.js
+++ bundler/cache.js
@@ -1,10 +1,12 @@
 var crypto = require('crypto'),
     util = require('util');
 
+require('longjohn')
+
 var level = require('level'),
     sublevel = require('level-sublevel'),
     levelbutt = require('level-scuttlebutt'),
-    expiryModel = require('expiry-model'),
+    ExpiryModel = require('expiry-model'),
     Model = require('scuttlebutt/model'),
     Bucket = require('scuttlebucket'),
     //cull = require('./npm-cull'),
@@ -29,11 +31,10 @@ Store.prototype.open = function (name, cb) {
   this.scuttles.open.call(this.scuttles, name, cb);
 };
 
-var Cache = function () {
-  Model.call(this);
-};
-util.inherits(Cache, Model);
-
+var Cache = function (opts) {
+  ExpiryModel.call(this);
+}
+util.inherits(Cache, ExpiryModel);
 Cache.prototype.hashfxn = defaultHashFxn;
 
 Cache.prototype.check = function (key, generate, cb) {
@@ -68,31 +69,37 @@ Cache.prototype.check = function (key, generate, cb) {
   }
 };
 
+function makeCache(name, opts) {
+  var SubCache = function () {
+    Cache.call(this, opts);
+    this.set('name', name);
+  }
+  util.inherits(SubCache, Cache);
+  return new SubCache();
+}
+
 var SECONDS = 1000,
     MINUTES = 60 * SECONDS,
     HOURS = 60 * MINUTES,
-    DAYS = 24 * HOURS;
+    DAYS = 24 * HOURS,
+    WEEKS = 7 * DAYS;
 
 module.exports = function (location) {
 
   var store = new Store(location);
 
   store.register('bundles', function () {
-    var bundles = new Cache();
-    bundles.set('name', 'bundles');
-    return bundles;
+    return makeCache('bundles', { maxAge: 4 * WEEKS });
   });
 
   store.register('builds', function () {
-    var builds = new Cache();
-    builds.set('name', 'builds');
+    var builds = makeCache('builds', { maxAge: 4 * WEEKS });
     builds.hashfxn = function (o) { return o.module; };
     return builds;
   });
 
   store.register('multibundles', function () {
-    var multis = new Cache();
-    multis.set('name', 'multibundles');
+    var multis = makeCache('multibundles', { maxAge: 1 * WEEKS });
     multis._hashfxn = multis.hashfxn;
     multis.hashfxn = function (o) {
       if (typeof o === 'string' && o.length === 24) {
@@ -105,8 +112,7 @@ module.exports = function (location) {
   });
 
   store.register('aliases', function () {
-    var aliases = new Cache({
-      name: 'aliases',
+    var aliases = makeCache('aliases', {
       maxAge: 1 * DAYS
     });
     aliases.hashfxn = function (o) {
