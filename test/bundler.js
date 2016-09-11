0830749d8a9e27c95f3538d8aee862f141b07976
Joshua Holbrook <josh.holbrook@gmail.com>
2016-09-11 14:28:41 -0400

    Add destroy hooks to caches, passing bundler tests

--- test/bundler.js
+++ test/bundler.js
@@ -1,9 +1,41 @@
 'use strict';
 
+const Promise = require('bluebird');
+const sinon = require('sinon');
 const tap = require('tap');
 
 const Bundler = require('../lib/bundler');
 
-tap.test('Bundler', { skip: true }, (t) => {
+const fakeBuilder = {
+  init: sinon.stub().returns(Promise.resolve('butts'))
+};
+
+let bundler;
+
+tap.plan(2);
+
+tap.test('Bundler constructor', (t) => {
+  t.doesNotThrow(() => {
+    bundler = new Bundler({
+      db: './cdn.db'
+    });
+  }, 'constructs successfully');
+
+  bundler._builder = fakeBuilder;
+
+  bundler._caches._destroy();
+
   t.end();
 });
+
+tap.test('Bundler init', (t) => {
+  bundler.init().then(() => {
+    t.ok('bundler init succeeded');
+    t.ok(fakeBuilder.init.calledOnce, 'builder init was called once');
+  }).catch((err) => {
+    t.fail(err, 'did not successfully init');
+  }).then(() => {
+    t.end();
+  });
+});
+
