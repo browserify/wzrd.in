8529ecee33a0e899f92e37f3c0a775ec9f321325
Joshua Holbrook <josh.holbrook@gmail.com>
2016-09-25 18:41:16 -0400

    Hide "new" purge routes

--- routes.js
+++ routes.js
@@ -10,19 +10,25 @@ const statusesHandler = require('./handlers/statuses');
 
 module.exports = function (app, bundler, config) {
   app.get('/bundle/:slug', singularHandler(bundler));
-  app.purge('/bundle/:slug', singularHandler(bundler, { purge: true }));
   app.get('/debug-bundle/:slug', singularHandler(bundler, { debug: true }));
-  app.purge('/debug-bundle/:slug', singularHandler(bundler, { debug: true, purge: true }));
   app.get('/standalone/:slug', singularHandler(bundler, { standalone: true }));
-  app.purge('/standalone/:slug', singularHandler(bundler, { standalone: true, purge: true }));
   app.get('/debug-standalone/:slug', singularHandler(bundler, { standalone: true, debug: true }));
-  app.purge('/debug-standalone/:slug', singularHandler(bundler, { standalone: true, debug: true, purge: true }));
 
   app.post('/multi', bodyParser.json(), multiHandlers.create(bundler));
   app.get('/multi/:bundle', multiHandlers.get(bundler));
-  app.purge('/multi/:bundle', multiHandlers.purge(bundler));
 
   app.get('/status/:bundle', statusesHandler(bundler));
 
+  // Purge routes.
+  // The PURGE method routes are dead code, For Now, since the "admin"
+  // interface is already being used by the mods, with proposed work to
+  // migrate to PURGE methods in future work.
   app.get('/admin/cull/:module', auth(config), cullHandler(bundler, config));
+  /*
+  app.purge('/bundle/:slug', singularHandler(bundler, { purge: true }));
+  app.purge('/debug-bundle/:slug', singularHandler(bundler, { debug: true, purge: true }));
+  app.purge('/standalone/:slug', singularHandler(bundler, { standalone: true, purge: true }));
+  app.purge('/debug-standalone/:slug', singularHandler(bundler, { standalone: true, debug: true, purge: true }));
+  app.purge('/multi/:bundle', multiHandlers.purge(bundler));
+  */
 }
