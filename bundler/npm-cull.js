67f628725832917406b22544fc512d6f9d916f81
Renée Kooi <renee@kooi.me>
2018-11-30 22:10:18 +0100

    Replace npm-publish-stream by changes-stream.

--- bundler/npm-cull.js
+++ bundler/npm-cull.js
@@ -1,20 +1,21 @@
-var NpmPublishStream = require('npm-publish-stream'),
+var ChangesStream = require('changes-stream'),
+    normalizeRegistryMetadata = require('normalize-registry-metadata'),
     log = require('minilog')('cull'),
     semver = require('semver');
 
 module.exports = function _cull(cache) {
   var aliases = cache.db,
-      pubStream = new NpmPublishStream({
-        refreshRate: 2 * 60 * 1000,
-        hostname: 'skimdb.npmjs.com',
-        port: 443,
-        protocol: 'https://'
+      pubStream = new ChangesStream({
+        db: 'https://replicate.npmjs.com',
+        include_docs: true,
+        since: 'now'
       });
 
   pubStream.on('data', function (data) {
     var module = data.id,
-        version = data.doc['dist-tags'].latest, // Breaks under force publishes
-        start = module,                         // of olde versions...
+        doc = normalizeRegistryMetadata(data.doc),
+        version = doc['dist-tags'].latest, // Breaks under force publishes
+        start = module,                    // of olde versions...
         end = start.slice(0, start.length - 1)     // TODO: Ask rvagg about this
               + String.fromCharCode(start.charCodeAt(start.length - 1) + 1);
 
@@ -22,7 +23,9 @@ module.exports = function _cull(cache) {
 
     stream.on('data', function (key) {
       console.log('cull stream on data', key);
-      var range = key.split('@').pop();
+      var range = /(.+)@/.exec(key);
+      if (range) range = range[1];
+      if (!range) return;
 
       if (semver.satisfies(version, range) || range == 'latest') {
         log('culling `' + module + '@' + range + '`...');
